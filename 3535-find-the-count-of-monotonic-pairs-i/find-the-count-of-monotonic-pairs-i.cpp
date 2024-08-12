1c64e159fe741e8ab8a71240541f6b27374370f0
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-08-12 23:11:13 +0530

    Time: 163 ms (58.33%) | Memory: 72.8 MB (41.67%) - LeetSync

--- 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.cpp
+++ 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
    int N=1e9+7;
-   long long solve(int i,int prev1,int prev2,vector<int>& nums,vector<vector<vector<long long>>>&dp){
+   long long solve(int i,int prev1,vector<int>& nums,vector<vector<long long>>&dp){
     if(i==nums.size())return 1;
-    if(dp[i][prev1][prev2]!=-1)return dp[i][prev1][prev2];
+    if(dp[i][prev1]!=-1)return dp[i][prev1];
 
     long long ans=0;
+    int prev2=50;
+    if(i>0)prev2=nums[i-1]-prev1;
     for(int j=prev1;j<=nums[i];++j){
-        if(nums[i]-j>=0 && nums[i]-j<=prev2)ans=(ans+solve(i+1,j,nums[i]-j,nums,dp))%N;
+        if(nums[i]-j>=0 && nums[i]-j<=prev2)ans=(ans+solve(i+1,j,nums,dp))%N;
     }
-    return dp[i][prev1][prev2]= ans;
+    return dp[i][prev1]= ans;
    }
     int countOfPairs(vector<int>& nums) {
        int n=nums.size();
-       vector<vector<vector<long long>>>dp(n,vector<vector<long long>>(51,vector<long long>(51,-1)));
-       long long ans=solve(0,0,50,nums,dp)%N;
+       vector<vector<long long>>dp(n,vector<long long>(51,-1));
+       long long ans=solve(0,0,nums,dp)%N;
        return (int)ans; 
     }
 };
