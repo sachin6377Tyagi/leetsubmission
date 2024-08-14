1afc2d9769301cc7d95dca637525e3a3e914a94a
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-08-14 17:32:37 +0530

    Time: 15 ms (98.78%) | Memory: 20.6 MB (43.73%) - LeetSync

---
+++ 690-employee-importance/employee-importance.cpp
@@ -0,0 +1,30 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+   int dfs(int node,unordered_map<int,bool>&vis,vector<Employee*> &emp,map<int,int>&mp){
+    int ans=emp[mp[node]]->importance;
+    vis[mp[node]]=1;
+    for(auto nbr:emp[mp[node]]->subordinates){
+       // if(vis[nbr]) continue;
+        int num=dfs(nbr,vis,emp,mp);
+        // num+=emp[nbr]->importance;
+        ans+=num;
+    }
+    return ans;
+   }
+    int getImportance(vector<Employee*> employees, int id) {
+        unordered_map<int,bool>vis;
+        map<int,int>mp;
+        for(int i=0;i<employees.size();++i)mp[employees[i]->id]=i;
+        return dfs(id,vis,employees,mp);
+    }
+};
