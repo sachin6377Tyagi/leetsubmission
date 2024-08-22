fd09d3afd0eb4b09125a6ce97d11fcfc7ae18f50
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-08-22 20:46:46 +0530

    Time: 9 ms (10.73%) | Memory: 10.4 MB (6.92%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n=s.size();
+        map<char,int>ind;
+        map<char,int>mp;
+        map<char,bool>vis;
+        for(int i=0;i<n;++i){
+            ind[s[i]]=i;
+            mp[s[i]]++;
+        }
+        stack<pair<char,int>>st;
+        for(int i=0;i<n;++i){
+            mp[s[i]]--;
+            while(!st.empty() && st.top().first>s[i] && mp[st.top().first]>0 && !vis[s[i]]){
+                vis[st.top().first]=0;
+                st.pop();
+            }
+            if(!vis[s[i]]){
+                st.push({s[i],i});
+                vis[s[i]]=1;
+            }
+        }
+        string ans="";
+        while(st.size()){
+            ans+=st.top().first;
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
