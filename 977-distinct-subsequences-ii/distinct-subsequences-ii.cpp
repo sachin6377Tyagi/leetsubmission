4deb13107e76a6c80bd1e27e2e153f316e865eef
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-09-19 22:39:41 +0530

    Time: 7 ms (32.97%) | Memory: 8.7 MB (55.27%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+       int n=s.size();
+       int N=1e9+7;
+       vector<long long>dp(n);
+       dp[0]=1;
+       for(int i=1;i<n;++i){
+        int j=i-1;
+        while(j>=0){
+            dp[i]=(dp[i]+dp[j])%N;
+            if(s[j]==s[i])break;
+            j--;
+        }
+        if(j==-1)dp[i]=(dp[i]+1)%N;
+       }
+       long long ans =0;
+       for(int i=0;i<n;++i)ans=(ans+dp[i])%N;
+       return ans;
+    }
+};
