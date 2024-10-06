caf689dc42cb183b76142c80da541e2a10c2c532
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-10-06 11:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (66.36%) - LeetSync

--- 1923-sentence-similarity-iii/sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/sentence-similarity-iii.cpp
@@ -1,76 +1,61 @@
 class Solution {
 public:
-    bool areSentencesSimilar(string sent1, string sent2) {
-        int n=sent1.size(),m=sent2.size();
-        if(n<m)swap(sent1,sent2);
-        unordered_set<string>st;
-        vector<string>v1,v2;
-        string temp="";
-        for(int i=0;i<sent1.size();++i){
-            if(sent1[i]==' '){
-                st.insert(temp);
-                v1.push_back(temp);
-                temp="";
-            }
-            else temp+=sent1[i];
+    bool areSentencesSimilar(string sentence1, string sentence2) {
+    
+    vector<string> s1;
+    vector<string> s2;
+
+    sentence1.push_back(' ');
+    sentence2.push_back(' ');
+
+    string ans = "";
+
+    for(int i=0; i<sentence1.size(); i++)
+    {
+        if(sentence1[i] == ' ')
+        {
+            s1.push_back(ans); 
+            ans = "";
         }
-        st.insert(temp);
-        v1.push_back(temp);
-        temp="";
-        for(int i=0;i<sent2.size();++i){
-            if(sent2[i]==' '){
-                if(st.find(temp)==st.end())return false;
-                v2.push_back(temp);
-                temp="";
-            }
-            else temp+=sent2[i];
+        else
+        {
+            ans.push_back(sentence1[i]);
         }
-        if(st.find(temp)==st.end())return false;
-        v2.push_back(temp);
-        int cnt=0,flag=0;
-        int i=0,j=0;
-        bool ans=1;
-        while(i<v1.size() && j<v2.size()){
-            if(v1[i]==v2[j]){
-                i++;
-                j++;
-                if(cnt)flag=1;
+    }
+        for(int i = 0; i<sentence2.size(); i++)
+        {
+            if(sentence2[i] == ' ')
+            {
+                s2.push_back(ans);
+                ans = "";
             }
-            else if(flag==0 && v1[i]!=v2[j]){
-                cnt++;
-                i++;
-            }
-            else if(flag){
-                ans=0;
-                break;
+            else
+            {
+                ans.push_back(sentence2[i]);
             }
         }
-        if(i==v1.size() && j<v2.size() && v1.size()==v2.size())return false;
-        if(i<v1.size() && flag==1)ans=0;
-        if(i==v1.size() && flag==1 && j<v2.size())ans=0;
-        if(!ans){
-            bool f1=1;
-            flag=0;
-            i=v1.size()-1,j=v2.size()-1;
-            int cnt=0;
-            while(i>=0 && j>=0){
-                if(v1[i]==v2[j]){
-                    i--,j--;
-                    if(cnt)flag=1;
-                }
-                else if(flag==0 && v1[i]!=v2[j]){
-                    i--;
-                    cnt++;
-                }
-                else if(flag){
-                    f1=0;
-                    break;
-                }
-            }
-            if(i>=0 && flag)f1=0;
-            if(i<0 && flag==1 && j>=0)f1=0;
-            ans=ans|f1;
+   
+       int m1 = 0, m2 = 0;
+       int n1 = s1.size() - 1, n2 = s2.size() - 1;
+
+       while(m1 <= n1 && m2 <= n2)
+       {
+        if(s1[m1] == s2[m2])
+        {
+            m1++;
+            m2++;
         }
-        return ans;
+        else if(s1[n1] == s2[n2])
+        {
+            n1--;
+            n2--;
+        }
+        else break;
+       
+       }
+    
+ 
+      return(n1 + 1 == m1 || n2 +1 == m2) ? true:false;
+
     }
 };
