5eb730fb9d0f31f8febc92c507acfdff6ceeb14a
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-10-07 15:41:43 +0530

    Time: 734 ms (25.00%) | Memory: 293.9 MB (10.71%) - LeetSync

---
+++ 3578-construct-2d-grid-matching-graph-layout/construct-2d-grid-matching-graph-layout.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+   void dfs1(int node,int col,unordered_map<int,vector<int>>&adj,vector<bool>&vis,vector<vector<int>>&vec){
+    vec[0][col]=node;
+    vis[node]=1;
+    for(auto nbr:adj[node]){
+        if(!vis[nbr])dfs1(nbr,col+1,adj,vis,vec);
+    }
+   }
+    vector<vector<int>> constructGridLayout(int n, vector<vector<int>>& edges) {
+       vector<int>deg(n,0);
+       unordered_map<int,vector<int>>adj;
+       for(int i=0;i<edges.size();++i){
+        adj[edges[i][0]].push_back(edges[i][1]);
+        adj[edges[i][1]].push_back(edges[i][0]);
+        deg[edges[i][0]]++;
+        deg[edges[i][1]]++;
+       }
+       vector<int>curr;
+       vector<bool>vis(n,0);
+       int x=min_element(deg.begin(),deg.end())-deg.begin();
+       curr.push_back(x);
+       vis[x]=1;
+       while(curr.size()){
+        int node=curr.back();
+        if(curr.size()>=2 && deg[node]==deg[x])break;
+        int nebr,dg=5;
+        for(auto nbr:adj[node]){
+            if(vis[nbr])continue;
+            if(deg[nbr]<dg){
+                dg=deg[nbr];
+                nebr=nbr;
+            }
+        }
+        curr.push_back(nebr);
+        vis[nebr]=1;
+       }
+       int col=curr.size(),row=n/col;
+       vector<vector<int>>ans(row);
+       ans[0]=curr;
+       for(int i=1;i<row;++i){
+        vector<int>temp;
+        for(int j=0;j<col;++j){
+            for(auto nbr:adj[ans[i-1][j]]){
+                if(!vis[nbr]){
+                    vis[nbr]=1;
+                    temp.push_back(nbr);
+                    break;
+                }
+            }
+        }
+        ans[i]=temp;
+       }
+       return ans;
+    }
+};
