4ef86058f3e64fb5c5239b59f666b57197fd65d8
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-10-19 01:55:06 +0530

    Time: 0 ms (100.00%) | Memory: 70.5 MB (70.61%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+// khadane algo
+    int maxSubArray(vector<int>& nums) {
+        int ans=-1e9;
+        int sum=0;
+        for(int i=0;i<nums.size();++i){
+            sum+=nums[i];
+            ans=max(ans,sum);
+            if(sum<0)sum=0;
+        }
+        return ans;
+    }
+};
