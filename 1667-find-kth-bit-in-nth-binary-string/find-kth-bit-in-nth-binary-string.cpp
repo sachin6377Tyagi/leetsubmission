921639c6380b9230ca92439212bbcd73792cdd43
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-10-20 14:44:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (64.26%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     char findKthBit(int n, int k) {
-        if (k==1) return '0';// base case
-        int sz=32-__builtin_clz(k);
-        int k0=(1<<sz)-k; // reflected number
-        if (k0==k) return '1';// k is a symmetry number
-    //    cout<<(1<<sz)<<", k="<<k<<endl;
-        return (findKthBit(n, k0)=='0')?'1':'0';
+        if(n==1)return '0';
+        int len=(1<<n)-1;
+        int mid=(ceil(len/2.0));
+        if(k<mid)return findKthBit(n-1,k);
+        else if(k==mid)return '1';
+        else{
+            char ch=findKthBit(n-1,len-(k-1));
+            if(ch=='0')return '1';
+            else return '0';
+        }
     }
 };
