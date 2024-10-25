cf611b0fccff9835709bb04aa8034ace87637202
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-10-25 16:23:27 +0530

    Time: 817 ms (7.30%) | Memory: 126.3 MB (11.39%) - LeetSync

---
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<pair<int,int>>divideArray(int lo,int hi,int n,vector<int>&nums){
+        vector<pair<int,int>>ans;
+        int len=(1<<n);
+        for(int i=0;i<len;++i){
+            pair<int,int>p={0,0};
+            for(int j=0;j<n;++j){
+                if(i&(1<<j)){
+                    p.first+=nums[j+lo];
+                    p.second++;
+                }
+            }
+            ans.push_back(p);
+        }
+        return ans;
+    }
+    int minimumDifference(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();++i)sum+=nums[i];
+        int n=nums.size();
+        n=n/2;
+        vector<pair<int,int>>left=divideArray(0,n,n,nums);
+        vector<pair<int,int>>right=divideArray(n,2*n,n,nums);
+        sort(right.begin(),right.end());
+        unordered_map<int,vector<int>>mp;
+        for(int i=0;i<right.size();++i){
+            mp[right[i].second].push_back(right[i].first);
+        }
+        // for(int i=0;i<=2;++i){
+        //     for(auto it:mp[i])cout<<it<<" ";
+        //     cout<<endl;
+        // }
+        int ans=1e9;
+        for(int i=0;i<left.size();++i){
+            int num=left[i].first;
+            int l1=left[i].second;
+            //cout<<num<<" "<<l1<<endl;
+            int ind=lower_bound(mp[n-l1].begin(),mp[n-l1].end(),sum/2-num)-mp[n-l1].begin();
+            //cout<<ind<<endl;
+            if(ind!=mp[n-l1].size()){
+                int sum1=num+mp[n-l1][ind];
+                int sum2=sum-sum1;
+                ans=min(ans,abs(sum1-sum2));
+            }
+        }
+        return ans;
+    }
+};
