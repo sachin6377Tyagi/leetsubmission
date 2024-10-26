720e86df1a98ef0cd2a622b7c6061075b59b2ab1
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-10-26 15:07:15 +0530

    Time: 2027 ms (5.12%) | Memory: 495.5 MB (5.12%) - LeetSync

--- 2757-count-of-integers/count-of-integers.cpp
+++ 2757-count-of-integers/count-of-integers.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
    int N=1e9+7;
-    long long solve(int ind,int sum,int flag,string &num,int mini,int maxi,vector<vector<vector<long long>>>&dp){
+    int solve(int ind,int sum,int flag,string &num,int mini,int maxi,vector<vector<vector<int>>>&dp){
         if(ind==num.size()){
             if(sum<=maxi && sum>=mini)return 1;
             return 0;
@@ -13,13 +13,13 @@ public:
             int x=flag?1:i!=end;
             ans=(ans+solve(ind+1,sum+i,x,num,mini,maxi,dp))%N;
         }
-        return dp[ind][sum][flag]= ans;
+        return dp[ind][sum][flag]= ans%N;
     }
     int count(string num1, string num2, int min_sum, int max_sum) {
-        vector<vector<vector<long long>>>dp1(23,vector<vector<long long>>(405,vector<long long>(2,-1)));
-        vector<vector<vector<long long>>>dp2(23,vector<vector<long long>>(405,vector<long long>(2,-1)));
-        long long ans1=solve(0,0,0,num1,min_sum,max_sum,dp1)%N;
-        long long ans2=solve(0,0,0,num2,min_sum,max_sum,dp2)%N;
+        vector<vector<vector<int>>>dp1(23,vector<vector<int>>(405,vector<int>(2,-1)));
+        vector<vector<vector<int>>>dp2(23,vector<vector<int>>(405,vector<int>(2,-1)));
+        int ans1=solve(0,0,0,num1,min_sum,max_sum,dp1)%N;
+        int ans2=solve(0,0,0,num2,min_sum,max_sum,dp2)%N;
         int flag=0,sum=0;
         for(int i=0;i<num1.size();++i)sum+=num1[i]-'0';
         if(sum>=min_sum && sum<=max_sum)flag=1;
