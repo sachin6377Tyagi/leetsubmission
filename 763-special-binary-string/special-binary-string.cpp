6c46d66803235e738598c20d8c51ce7ab819e255
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-11-01 23:23:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (8.35%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        int n=s.size();
+        int cnt=0,start=0;
+        vector<string>vec;
+        for(int i=0;i<n;++i){
+            if(s[i]=='1')cnt++;
+            else cnt--;
+            if(cnt==0){
+                string temp="1"+makeLargestSpecial(s.substr(start+1,i-start-1))+"0";
+                vec.push_back(temp);
+                start=i+1;
+            }
+        }
+        string ans="";
+        sort(vec.rbegin(),vec.rend());
+        for(auto it:vec)ans+=it;
+        return ans;
+    }
+};
