dc15aa755ee994f40d3030791cfc70aaf3a0565d
sachin6377Tyagi <100961702+sachin6377Tyagi@users.noreply.github.com>
2024-11-01 11:35:37 +0530

    Time: 39 ms (84.85%) | Memory: 10.8 MB (98.16%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    long long dp[105][105][105];
-    long long solve(int i,int j,int lim,vector<int>& robot, vector<vector<int>>& factory){
-        if(i==robot.size())return 0;
-        if(j==factory.size())return 1e15;
-        if(dp[i][j][lim]!=-1)return dp[i][j][lim];
-        long long ans=1e15;
-        if(j+1<factory.size())ans=min(ans,solve(i,j+1,factory[j+1][1],robot,factory));
-       if(lim>0) {
-            ans=min(ans,abs(robot[i]-factory[j][0])+solve(i+1,j,lim-1,robot,factory));
-            //ans=min(ans,solve(i,j+1,factory[j][1],robot,factory));
-        }
-        return dp[i][j][lim]= ans;
+  long long dp[105][105];
+   long long solve(int i,int j,vector<int>& robot, vector<vector<int>>& factory){
+    if(i==robot.size())return 0;
+    if(j==factory.size())return 1e15;
+    if(dp[i][j]!=-1)return dp[i][j];
+    long long ans=1e15;
+    ans=min(ans,solve(i,j+1,robot,factory));
+    long long sum=0;
+    for(int k=i;k<robot.size() && k<i+factory[j][1];++k){
+        sum+=abs(robot[k]-factory[j][0]);
+        ans=min(ans,sum+solve(k+1,j+1,robot,factory));
     }
+    return dp[i][j]= ans;
+   }
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
-        sort(robot.begin(), robot.end());
-        sort(factory.begin(), factory.end());
-        
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, factory[0][1],robot, factory);
+        sort(robot.begin(),robot.end());
+        sort(factory.begin(),factory.end());
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,robot,factory);
     }
 };
-
